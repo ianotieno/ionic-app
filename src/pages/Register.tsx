e5b3b71eac363328c3f499a253eee38f9c673bb4
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-09-20 13:51:22 +0300

    update

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,20 +1,39 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react"
+import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar } from "@ionic/react"
+import { checkmarkDoneCircleOutline } from "ionicons/icons";
 import React from "react";
 const Register: React.FC=()=>{
+    const doRegister=(event:any)=>{
+        event.preventDefault();
+        console.log('doLogin');
+    }
     return(
         <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>
-                        page Title
-                    </IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
-             register ui goes here
-            </IonContent>
-        </IonPage>
-    );
+        <IonHeader>
+            <IonToolbar >
+                <IonTitle>
+                   Create Account
+                </IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent>
+            
+          <IonCard>
+            <IonCardContent>
+                <form onSubmit={doRegister}>
+              <IonInput label="Email" type='email' placeholder="ianotieno23@gmail.com" fill="outline" labelPlacement="floating"></IonInput>
+              <IonInput className="ion-margin-top" label="Password" type='password' placeholder="Password" fill="outline" labelPlacement="floating"></IonInput>
+              <IonButton className="ion-margin-top" type="submit" color={'tertiary'} expand="block">
+              <IonIcon icon={checkmarkDoneCircleOutline} slot="end"/>
+              Create Account</IonButton>
+              
+                </form>
+            </IonCardContent>
+            </IonCard>
+        </IonContent>
+    </IonPage>
+);
 };
+
+
 
 export default Register;
