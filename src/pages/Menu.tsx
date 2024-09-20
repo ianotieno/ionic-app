87922a19d7c4d633e7108c540d121c6091c0f4f6
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-09-21 00:30:56 +0300

    update

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,18 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonMenu, IonPage, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { Redirect, Route } from 'react-router';
+import List from './List';
+import Setting from './Setting';
 
 const Menu: React.FC = () => {
 
     return (
         <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Menu</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
-                UI goes here...
-            </IonContent>
+            <IonMenu contentId='main'>
+                <IonHeader>
+                    <IonToolbar>
+                  <IonTitle>Menu</IonTitle> 
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className='ion-padding'>Menu Items</IonContent>
+            </IonMenu>
+            <IonRouterOutlet id='main'>
+            <Route exact path="/app/list" component={List}/>
+            <Route path="/app/settings" component={Setting}/>
+            <Route exact path="/app">
+                <Redirect to="/app/list"/>
+            </Route>
+            </IonRouterOutlet>
         </IonPage>
     );
 };
--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,22 +1,15 @@
 import { IonContent, IonHeader, IonMenu, IonPage, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { Redirect, Route } from 'react-router';
+import List from './List';
 
 const Setting: React.FC = () => {
 
     return (
         <IonPage>
-            <IonMenu contentId='main'>
-                <IonHeader>
-                    <IonToolbar>
-                  <IonTitle>Menu</IonTitle> 
-                    </IonToolbar>
-                </IonHeader>
-                <IonContent className='ion-padding'>UI goes here</IonContent>
-            </IonMenu>
-            <IonRouterOutlet id='main'>
-
-            </IonRouterOutlet>
+            
         </IonPage>
+        
     );
 };
 
