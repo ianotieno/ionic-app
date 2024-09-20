91d49d7152f1345d203d82a9072d3a633e9773a7
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-09-20 14:31:46 +0300

    swiper

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,12 +1,17 @@
 import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar, useIonRouter } from "@ionic/react"
 import React from "react";
-
+import {Swiper,SwiperSlide} from 'swiper/react'
+import 'swiper/css'
 interface ContainerProps{
     onFinish:()=>void;
 }
 const Intro: React.FC<ContainerProps>=()=>{
    
-    return<div>test</div>;
+    return(
+        <Swiper>
+            <SwiperSlide></SwiperSlide>
+        </Swiper>
+    )
 }
 
 export default Intro
