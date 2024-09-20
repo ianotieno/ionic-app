0ed4826cd756844869d269ea82223afaa512fc61
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-09-20 12:30:08 +0300

    route

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { IonCard, IonCardContent, IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar } from "@ionic/react"
+import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar } from "@ionic/react"
 import React from "react";
 const Login: React.FC=()=>{
     const doLogin=(event:any)=>{
@@ -19,8 +19,9 @@ const Login: React.FC=()=>{
                 <IonCardContent>
                     <form onSubmit={doLogin}>
                   <IonInput label="Email" type='email' placeholder="ianotieno23@gmail.com" fill="outline" labelPlacement="floating"></IonInput>
-                  <IonInput label="Password" type='password' placeholder="Password" fill="outline" labelPlacement="floating"></IonInput>
-                  
+                  <IonInput className="ion-margin-top" label="Password" type='password' placeholder="Password" fill="outline" labelPlacement="floating"></IonInput>
+                  <IonButton className="ion-margin-top" type="submit" color={'tertiary'} expand="block">Login</IonButton>
+                  <IonButton color={'secondary'} routerLink="/register" className="ion-margin-top" type="button" expand="block">Create Account</IonButton>
                     </form>
                 </IonCardContent>
             </IonCard>
