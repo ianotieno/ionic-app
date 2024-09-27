360d040d32fdb043ee0f3f2462e5c2f13ac1f53a
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-09-27 10:23:04 +0300

    update

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCardHeader, IonCardSubtitle, IonCardTitle, IonAvatar, IonImg, IonGrid, IonRow, IonCol, IonIcon, IonAlert, IonCard } from '@ionic/react';
+import {
+    IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCardHeader, IonCardSubtitle, IonCardTitle,
+    IonAvatar, IonImg, IonGrid, IonRow, IonCol, IonIcon, IonAlert, IonCard, IonCardContent, IonItem,
+    IonSkeletonText, IonLabel
+} from '@ionic/react';
 import { exitOutline, flame, micOutline, peopleOutline } from 'ionicons/icons';
 
 const Rooms: React.FC = () => {
     const location = useLocation<any>();
     const [loading, setLoading] = useState<boolean>(true);
     const history = useHistory();  // Get history object to navigate programmatically
-    const { speaker, listeners, title, subtitle,subtitle2, icon } = location.state || { speaker: null, listeners: [], title: '', subtitle: '',subtitle2:'', icon: [] };
+    const { speaker, listeners, title, subtitle, subtitle2, icon } = location.state || { speaker: null, listeners: [], title: '', subtitle: '', subtitle2: '', icon: [] };
     
     const [showAlert, setShowAlert] = useState(false);  // State to control alert visibility
+
+    // Simulate data loading for 2 seconds
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(false), 2000);
+        return () => clearTimeout(timer);
+    }, []);
 
     // Handle exit button click
     const handleExitClick = () => {
@@ -38,80 +48,104 @@ const Rooms: React.FC = () => {
             </IonHeader>
 
             <IonContent className="ion-padding">
-                
-                <IonCard className="happening-now-card">
-                    <IonCardHeader>
-                        <IonCardSubtitle><IonIcon style={{
-            backgroundColor: 'green',
-            borderRadius: '50%'
-          }} icon={flame} /> {title}</IonCardSubtitle>
-                        <IonCardTitle> {subtitle}</IonCardTitle>
-                        <IonCardSubtitle> {subtitle2}</IonCardSubtitle>
-                    </IonCardHeader>
+                {loading ? (
+                    [...Array(6)].map((_, index) => (
+                        <IonCard key={index}>
+                            <IonCardContent className="ion-no-padding">
+                                <IonCardHeader>
+                                    <IonCardSubtitle>
+                                        <IonSkeletonText animated />
+                                    </IonCardSubtitle>
+                                </IonCardHeader>
+                                <IonItem lines="none">
+                                    <IonAvatar slot="start">
+                                        <IonSkeletonText animated />
+                                    </IonAvatar>
+                                    <IonLabel>
+                                        <IonSkeletonText animated style={{ width: '150px' }} />
+                                        <p style={{ fontSize: '10px' }}>
+                                            <IonSkeletonText animated />
+                                        </p>
+                                    </IonLabel>
+                                </IonItem>
+                            </IonCardContent>
+                        </IonCard>
+                    ))
+                ) : (
+                    <>
+                        <IonCard className="happening-now-card">
+                            <IonCardHeader>
+                                <IonCardSubtitle>
+                                    <IonIcon style={{ backgroundColor: 'green', borderRadius: '50%' }} icon={flame} /> {title}
+                                </IonCardSubtitle>
+                                <IonCardTitle> {subtitle}</IonCardTitle>
+                                <IonCardSubtitle> {subtitle2}</IonCardSubtitle>
+                            </IonCardHeader>
 
-                    {/* Display the speaker */}
-                    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      
-    }}>
-      <IonAvatar style={{ position: 'relative', display: 'inline-block' }}>
-        <IonImg
-          style={{ border: '2px solid green' }}
-          src={speaker?.user.picture.thumbnail}
-          alt="Speaker Avatar"
-        />
-        <IonIcon
-          icon={micOutline}
-          style={{
-            position: 'absolute',
-            bottom: '5px',
-            right: '5px',
-            backgroundColor: 'white',
-            borderRadius: '50%',
-            padding: '2px',
-            zIndex: 1 // Ensure the icon is above the image
-          }}
-        />
-      </IonAvatar>
-    </div>
-                    <h2> <IonIcon icon={micOutline} /> Speaker: {speaker?.user.name.first} {speaker?.user.name.last}</h2>
-                </IonCard>
-               
-                {/* Display listeners with count */}
-                <h3> <IonIcon icon={peopleOutline} /> Audience: ({listeners.length})</h3> {/* Here, we display the count */}
-                <IonGrid>
-                    <IonRow>
-                        {listeners.map((listener: any, index: number) => (
-                            <IonCol key={index} size="auto">
-                                <IonAvatar>
-                                    <IonImg src={listener.user.picture.thumbnail} />
+                            {/* Display the speaker */}
+                            <div style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}>
+                                <IonAvatar style={{ position: 'relative', display: 'inline-block' }}>
+                                    <IonImg
+                                        style={{ border: '2px solid green' }}
+                                        src={speaker?.user.picture.thumbnail}
+                                        alt="Speaker Avatar"
+                                    />
+                                    <IonIcon
+                                        icon={micOutline}
+                                        style={{
+                                            position: 'absolute',
+                                            bottom: '5px',
+                                            right: '5px',
+                                            backgroundColor: 'white',
+                                            borderRadius: '50%',
+                                            padding: '2px',
+                                            zIndex: 1 // Ensure the icon is above the image
+                                        }}
+                                    />
                                 </IonAvatar>
-                                <p>{listener.user.name.first}</p>
-                            </IonCol>
-                        ))}
-                    </IonRow>
-                </IonGrid>
-            </IonContent>
+                            </div>
+                            <h2><IonIcon icon={micOutline} /> Speaker: {speaker?.user.name.first} {speaker?.user.name.last}</h2>
+                        </IonCard>
+                        
+                        {/* Display listeners with count */}
+                        <h3><IonIcon icon={peopleOutline} /> Audience: ({listeners.length})</h3> {/* Display audience count */}
+                        <IonGrid>
+                            <IonRow>
+                                {listeners.map((listener: any, index: number) => (
+                                    <IonCol key={index} size="auto">
+                                        <IonAvatar>
+                                            <IonImg src={listener.user.picture.thumbnail} />
+                                        </IonAvatar>
+                                        <p>{listener.user.name.first}</p>
+                                    </IonCol>
+                                ))}
+                            </IonRow>
+                        </IonGrid>
+                    </>
+                )}
 
-            {/* Exit confirmation pop-up */}
-            <IonAlert
-                isOpen={showAlert}
-                onDidDismiss={() => setShowAlert(false)}  // Close the alert when dismissed
-                header={'Exit Room'}
-                message={'Are you sure you want to exit the room?'}
-                buttons={[
-                    {
-                        text: 'Cancel',
-                        role: 'cancel',
-                    },
-                    {
-                        text: 'Confirm',
-                        handler: handleExitConfirm
-                    }
-                ]}
-            />
+                {/* Exit confirmation pop-up */}
+                <IonAlert
+                    isOpen={showAlert}
+                    onDidDismiss={() => setShowAlert(false)}  // Close the alert when dismissed
+                    header={'Exit Room'}
+                    message={'Are you sure you want to exit the room?'}
+                    buttons={[
+                        {
+                            text: 'Cancel',
+                            role: 'cancel',
+                        },
+                        {
+                            text: 'Confirm',
+                            handler: handleExitConfirm
+                        }
+                    ]}
+                />
+            </IonContent>
         </IonPage>
     );
 };
